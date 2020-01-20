137df2c6c0bf3898145bc485f1014146bf8a81c6
hakku <hakku@localhost.localdomain>
2020-01-20 19:13:46 +0100

    added second_index.py

--- checkio/second_index.py
+++ checkio/second_index.py
@@ -1,6 +1,11 @@
-def second_index(text: str, symbol: str) -> [int, None]:
+def second_index(text,symbol):
     """
         returns the second index of a symbol in a given text
     """
     # your code here
-    return 0
+    if text.count(symbol)<2:
+        return None
+    pri=text.find(symbol)
+    sec=text.find(symbol,pri+1)
+    return sec
+print(second_index("sims", "s"))
