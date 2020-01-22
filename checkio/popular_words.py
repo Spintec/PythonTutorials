98f4f01a93b2c2b8db755d3e7724afb3f50afe0a
Spintec <sindome123@gmail.com>
2020-01-22 21:05:46 +0100

    added popular_words

---
+++ checkio/popular_words.py
@@ -0,0 +1,18 @@
+def popular_words(text: str, words: list) -> dict:
+    dictz = {x: 0 for x in words}
+    text = text.lower()
+    for word in text.split():
+        if word in dictz:
+            dictz[word] = dictz[word] + 1
+    return dictz
+
+
+
+
+
+print(popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']))
