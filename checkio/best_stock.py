5a7d91328854b4c59d3482741aa1b3623490866d
Spintec <sindome123@gmail.com>
2020-01-22 20:28:22 +0100

    added prints on best_stock and correct_sentence

--- checkio/best_stock.py
+++ checkio/best_stock.py
@@ -1,2 +1,5 @@
 def best_stock(a):
     return list(a.keys())[list(a.values()).index(max(a.values()))]
+
+print(best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}))
+print(best_stock({"CAC": 91.1, "ATX": 1.01, "TASI": 120.9}))
--- checkio/correct_sentence.py
+++ checkio/correct_sentence.py
@@ -4,3 +4,9 @@ def correct_sentence(text):
             text=text[0:i]
             break
     return text[0].upper() + text[1:]+"."
+
+print(correct_sentence("greetings, friends"))
+print(correct_sentence("Greetings, friends"))
+print(correct_sentence("Greetings, friends."))
+print(correct_sentence("hi"))
+print(correct_sentence("welcome to New York"))
