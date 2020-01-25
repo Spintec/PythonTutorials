42743dd4e40878aef6a145dc54f30a96c8b8edd2
Spintec <sindome123@gmail.com>
2020-01-25 14:08:22 +0100

    +elem

---
+++ checkio/elem/between_markers_simplified.py
@@ -0,0 +1,10 @@
+def between_markers(text: str, begin: str, end: str) -> str:
+    """
+        returns substring between two given markers
+    """
+    return text[(text.index(begin)+1):text.index(end)]
+
+print(between_markers('What is >apple<', '>', '<'))
+print(between_markers('What is [apple]', '[', ']'))
+print(between_markers('>apple<', '>', '<'))
+print(between_markers('What is >an apple<', '>', '<'))
